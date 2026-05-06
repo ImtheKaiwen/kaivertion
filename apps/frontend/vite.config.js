66a995ac43dd6b50ac6a06bbb28e1244cd6e1907
Kerem <bucocuknereli@gmail.com>
2026-05-06 16:10:46 +0300

    news

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -5,11 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: [
+      'reatha-miliary-tattily.ngrok-free.dev',
+      '.ngrok-free.dev'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
